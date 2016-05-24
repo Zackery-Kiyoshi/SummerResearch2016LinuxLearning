ab6e08564e070db27b7749546bef1938c1435c79
Zackery-Kiyoshi <zkurimab@trinity.edu>
2016-05-24 15:34:48 -0500

    adding web stuff pre moving to htcdocs

---
+++ Web/MyJqueryTerminal/Test.php
@@ -0,0 +1,75 @@
+
+<!DOCTYPE HTML>
+<head>
+    <title> Testing JQuery Terminal </title>
+    <script src="js/jquery.terminal-0.10.8.min.js"></script>
+    <script src="js/jquery.mousewheel-min.js"></script>
+    <link href="css/jquery.terminal-0.10.8.min.css" rel="stylesheet"/>
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.terminal/0.10.8/js/jquery.terminal.min.js"></script>
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/jquery.terminal/0.10.8/css/jquery.terminal.min.css" rel="stylesheet"/>
+
+</head>
+
+<Body>
+    <p> 
+        test
+    </p>
+
+
+	<?php
+require('json_rpc.php');
+ 
+class Demo {
+  static $login_documentation = "return auth token";
+  public function login($user, $passwd) {
+    if (strcmp($user, 'demo') == 0 &&
+        strcmp($passwd, 'demo') == 0) {
+      // If you need to handle more than one user you can
+      // create new token and save it in database
+      return md5($user . ":" . $passwd);
+    } else {
+      throw new Exception("Wrong Password");
+    }
+  }
+ 
+  static $ls_documentation = "list directory if token is" .
+     " valid";
+  public function ls($token, $path) {
+    if (strcmp(md5("demo:demo"), $token) == 0) {
+      if (preg_match("/\.\./", $path)) {
+        throw new Exception("No directory traversal Dude");
+      }
+      $base = preg_replace("/(.*\/).*/", "$1",
+                           $_SERVER["SCRIPT_FILENAME"]);
+      $path = $base . ($path[0] != '/' ? "/" : "") . $path;
+      $dir = opendir($path);
+      while($name = readdir($dir)) {
+        $fname = $path."/".$name;
+        if (!is_dir($name) && !is_dir($fname)) {
+          $list[] = $name;
+        }
+      }
+      closedir($dir);
+      return $list;
+    } else {
+      throw new Exception("Access Denied");
+    }
+  }
+ 
+  static $whoami_documentation = "return user information";
+  public function whoami() {
+    return array(
+        "user-agent" => $_SERVER["HTTP_USER_AGENT"],
+        "your ip" => $_SERVER['REMOTE_ADDR'],
+        "referer" => $_SERVER["HTTP_REFERER"],
+        "request uri" => $_SERVER["REQUEST_URI"]);
+  }
+}
+ 
+handle_json_rpc(new Demo());
+?>
+  
+
+</Body>
+
